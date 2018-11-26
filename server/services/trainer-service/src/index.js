8baa78baadb510267f2d25695954a498fe8773a3
mick-roper <mick.roper@dazn.com>
2018-11-26 15:34:44 +0000

    added express

--- server/services/trainer-service/src/index.js
+++ server/services/trainer-service/src/index.js
@@ -1,1 +1,7 @@
-console.log('hello')
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3333;
+
+app.listen(port, () => console.log(`server listening on port ${port}`))
