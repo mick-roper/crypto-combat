88ac52544ba2146cde7f2cd649a280f09d71c391
mick-roper <mick.roper@dazn.com>
2018-11-26 15:43:25 +0000

    begun adding the trainer service

--- server/services/trainer-service/src/index.js
+++ server/services/trainer-service/src/index.js
@@ -4,4 +4,16 @@ const app = express();
 
 const port = process.env.PORT || 3333;
 
-app.listen(port, () => console.log(`server listening on port ${port}`))
+app.get('/:userId', (req, res) => {
+  const { userId } = req.params;
+
+  if (!userId) {
+    res
+      .status(400)
+      .send({
+        message: 'userId must be provided',
+      });
+  }
+});
+
+app.listen(port, () => console.log(`server listening on port ${port}`)); // eslint-disable-line
---
+++ server/services/trainer-service/src/trainer-service.js
@@ -0,0 +1,7 @@
+module.exports.getTrainerByUserId = async (userId) => {
+  throw new Error('not implemented');
+};
+
+module.exports.createTrainerFor = async (userId) => {
+  throw new Error('not implemented');
+};
