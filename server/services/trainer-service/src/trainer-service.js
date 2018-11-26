70e6136f1292d93f45c98b0dc1f695e7e435eb5e
mick-roper <mick.roper@dazn.com>
2018-11-26 15:51:47 +0000

    added null guard

--- server/services/trainer-service/src/trainer-service.js
+++ server/services/trainer-service/src/trainer-service.js
@@ -1,7 +1,15 @@
 module.exports.getTrainerByUserId = async (userId) => {
+  if (!userId) {
+    throw new Error('userId is not defined');
+  }
+
   throw new Error('not implemented');
 };
 
 module.exports.createTrainerFor = async (userId) => {
+  if (!userId) {
+    throw new Error('userId is not defined');
+  }
+
   throw new Error('not implemented');
 };
---
+++ server/services/trainer-service/src/trainer-service.test.js
@@ -0,0 +1,27 @@
+const service = require('./trainer-service');
+
+describe('getTrainerByUserId', () => {
+  test('throws if no userId', async () => {
+    try {
+      await service.getTrainerByUserId();
+    } catch (e) {
+      const { message } = e;
+      expect(message).toMatch('userId is not defined');
+    }
+
+    expect.assertions(1);
+  });
+});
+
+describe('createTrainerFor', () => {
+  test('throws if no userId', async () => {
+    try {
+      await service.createTrainerFor();
+    } catch (e) {
+      const { message } = e;
+      expect(message).toMatch('userId is not defined');
+    }
+
+    expect.assertions(1);
+  });
+});
