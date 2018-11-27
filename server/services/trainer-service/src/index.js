47c5a93fc121c12e753bad1e5a5ee43e4c5290c2
mick-roper <mick.roper@dazn.com>
2018-11-27 08:30:24 +0000

    added tests for the store

--- server/services/trainer-service/src/index.js
+++ server/services/trainer-service/src/index.js
@@ -2,18 +2,21 @@ const express = require('express');
 
 const app = express();
 
+const service = require('./trainer-service');
+
 const port = process.env.PORT || 3333;
 
-app.get('/:userId', (req, res) => {
+app.get('/:userId', async (req, res) => {
   const { userId } = req.params;
 
   if (!userId) {
-    res
-      .status(400)
-      .send({
-        message: 'userId must be provided',
-      });
+    res.status(400).send({ message: 'userId must be provided' });
+    return;
   }
+
+  const data = await service.getTrainerByUserId(userId);
+
+  res.status(200).send(data);
 });
 
 app.listen(port, () => console.log(`server listening on port ${port}`)); // eslint-disable-line
