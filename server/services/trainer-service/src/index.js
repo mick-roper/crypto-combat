e2950be3adda1be4ebac689f6e3eb243101eeeaf
mick-roper <mick.roper@dazn.com>
2018-11-27 08:32:44 +0000

    added interface endpoints

--- server/services/trainer-service/src/index.js
+++ server/services/trainer-service/src/index.js
@@ -16,7 +16,24 @@ app.get('/:userId', async (req, res) => {
 
   const data = await service.getTrainerByUserId(userId);
 
-  res.status(200).send(data);
+  res.status(200)
+    .json(data)
+    .send();
+});
+
+app.post('/:userId', async (req, res) => {
+  const { userId } = req.params;
+
+  if (!userId) {
+    res.status(400).send({ message: 'userId must be provided' });
+    return;
+  }
+
+  const data = await service.createTrainerFor(userId);
+
+  res.status(200)
+    .json(data)
+    .send();
 });
 
 app.listen(port, () => console.log(`server listening on port ${port}`)); // eslint-disable-line
