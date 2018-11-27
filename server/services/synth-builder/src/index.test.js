172c03f0a7fa6d46b4c5b181df36564d1e2b2c56
mick-roper <mick.roper@dazn.com>
2018-11-27 14:02:51 +0000

    added tests

--- server/services/synth-builder/src/index.test.js
+++ server/services/synth-builder/src/index.test.js
@@ -1,4 +1,7 @@
+const { createSynth } = require('./processor');
 const { handler } = require('./index');
+
+jest.mock('./processor');
 
 describe('handler', () => {
   test('returns error obj if invalid data', () => {
@@ -7,5 +10,19 @@ describe('handler', () => {
     const res = handler(data);
 
     expect(res).toEqual({ statusCode: 400, message: 'no data' });
+  });
+
+  test('returns OK response', () => {
+    const event = {
+      body: JSON.stringify({ hello: 'world' }),
+    };
+
+    const data = {};
+
+    createSynth.mockImplementation(() => data);
+
+    const res = handler(event);
+
+    expect(res).toEqual({ statusCode: 200, data });
   });
 });
