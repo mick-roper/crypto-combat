b235dd9d77e82118b2ba00676524e8ab51dd64e4
mick-roper <mick.roper@dazn.com>
2018-12-04 10:34:11 +0000

    added jest config

--- server/services/synth-merger/src/index.test.js
+++ server/services/synth-merger/src/index.test.js
@@ -26,4 +26,27 @@ describe('handler', () => {
     expect(statusCode).toEqual(200);
     expect(data).toEqual(mergeResult);
   });
+
+  test('returns 200 with a valid payload', () => {
+    const p1 = 'abc';
+    const p2 = '123';
+    const mutagen = 0;
+    const event = {
+      message: JSON.stringify({ p1, p2, mutagen }),
+    };
+    const mergeResult = { c1: 'abc', c2: 'def' };
+
+    mutate.mockImplementation((a, b, c) => {
+      expect(a).toEqual(p1);
+      expect(b).toEqual(p2);
+      expect(c).toEqual(mutagen);
+      return mergeResult;
+    });
+
+    const { statusCode, body } = handler(event);
+    const data = JSON.parse(body);
+
+    expect(statusCode).toEqual(200);
+    expect(data).toEqual(mergeResult);
+  });
 });
