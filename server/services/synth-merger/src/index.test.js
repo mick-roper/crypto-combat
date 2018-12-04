6f5ff51ade2b7082ac56153a0f2550302717a4bc
mick-roper <mick.roper@dazn.com>
2018-12-04 10:42:23 +0000

    increased test coverage

--- server/services/synth-merger/src/index.test.js
+++ server/services/synth-merger/src/index.test.js
@@ -27,26 +27,37 @@ describe('handler', () => {
     expect(data).toEqual(mergeResult);
   });
 
-  test('returns 200 with a valid payload', () => {
-    const p1 = 'abc';
-    const p2 = '123';
-    const mutagen = 0;
-    const event = {
-      message: JSON.stringify({ p1, p2, mutagen }),
-    };
-    const mergeResult = { c1: 'abc', c2: 'def' };
+  describe('validation', () => {
+    const testCases = [
+      {
+        name: 'no p1 property',
+        args: {
+          p2: 'abc',
+          mutagen: 0,
+        },
+        expectedBody: 'no p1 property',
+      },
+      {
+        name: 'no p1 property',
+        args: {
+          p1: 'abc',
+          mutagen: 0,
+        },
+        expectedBody: 'no p2 property',
+      },
+    ];
 
-    mutate.mockImplementation((a, b, c) => {
-      expect(a).toEqual(p1);
-      expect(b).toEqual(p2);
-      expect(c).toEqual(mutagen);
-      return mergeResult;
+    testCases.forEach((t) => {
+      test(t.name, () => {
+        const event = {
+          message: JSON.stringify(t.args),
+        };
+
+        const { statusCode, body } = handler(event);
+
+        expect(statusCode).toEqual(400);
+        expect(body).toEqual(t.expectedBody);
+      });
     });
-
-    const { statusCode, body } = handler(event);
-    const data = JSON.parse(body);
-
-    expect(statusCode).toEqual(200);
-    expect(data).toEqual(mergeResult);
   });
 });
